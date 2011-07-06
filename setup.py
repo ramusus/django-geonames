6a2a18a902b52b1a7808b11822632ac6a069b365
ram <ramusus@gmail.com>
2011-07-06 21:55:22 +0400

    moved version to __init__

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-geonames',
-      version='1.0',
+      version=__import__('geonames').__version__,
       description='Fork of official GeoDjango geonames application refactored and adopted for Django 1.2.1',
       author='Justin Bronn',
       author_email='jbronn@geodjango.org',
